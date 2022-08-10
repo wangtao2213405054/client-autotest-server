c29ab3b7f1ab470dcd09ffdf05050e3c89bc1926
wangtao <tao.wang@davincimotor.com>
2022-08-10 17:23:01 +0800

    add setup code

--- setup.py
+++ setup.py
@@ -1,2 +1,26 @@
 # _author: Coke
 # _date: 2022/8/10 14:02
+
+from setuptools import find_packages, setup
+
+setup(
+    name='application',
+    description='this is the client autotest platform server',
+    url='https://github.com/wangtao2213405054/client-autotest-server',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask-sqlalchemy',
+        'flask',
+        'redis',
+        'flask-wtf',
+        'pymysql',
+        'flask_socketio',
+        'eventlet',
+        'authlib',
+        'requests',
+        'sqlalchemy'
+    ]
+)
