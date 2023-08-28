a4aff2451c4add4b765aa29a5108aa795a59ac5a
Coke <tao.wang@davincimotor.com>
2023-08-28 17:23:15 +0800

    fix: bug fix

--- application/utils/mock.py
+++ application/utils/mock.py
@@ -6,6 +6,11 @@ import re
 
 
 def decouple(data: str):
+    """
+    使用正则表达式匹配出对应的数据信息
+    :param data: 要匹配的正则表达式
+    :return:
+    """
     pattern = r"\{\{\s*([^\s|]+)\s+('[^']*')(?:\s*,\s*([^|]+))?\s*(?:\|([^%]+))*\s*(?:\|([^%]+))*\s*\}\}"
 
     matches = re.match(pattern, data)
@@ -33,14 +38,49 @@ def decouple(data: str):
     return map_var, function_name_var, function_args, function_var
 
 
+def apply_string_function(data: str, mapping: str) -> str:
+    """
+    处理正则函数, 如果存在对应的函数则进行处理, 否则返回原始数据
+    :param data: 要处理的字符串
+    :param mapping: 字符串的函数映射
+    :return:
+    """
+    if hasattr(data, mapping):
+        string_function = getattr(data, mapping)
+        if callable(string_function):
+            return string_function()
+
+    return data
+
+
+def length(data):
+    """
+    获取数据长度
+    :param data: 要获取的数据
+    :return:
+    """
+    return len(data)
+
+
+def section(data: str, start: int = 0, end: int = 1):
+    """
+    数据切片
+    :param data: 原始数据
+    :param start: 开始切片位置
+    :param end: 结束切片位置
+    :return:
+    """
+    return data[start: end]
+
+
 class MockFaker:
     """ 生成测试数据 """
 
     def __init__(self):
         self.fake = Faker()
 
-    def cparagraph(self, length=15):
-        self.fake = Faker('zh_CN')
+    def cparagraph(self, length=50):
+        self.fake = Faker('zh-CN')
         text = self.fake.text()  # 生成随机文本
         while len(text) < length:
             text += '' + self.fake.text()  # 继续生成随机文本，直到达到指定长度
@@ -48,5 +88,5 @@ class MockFaker:
 
 
 if __name__ == '__main__':
-    datas = "{{ mock 'cparagraph' }}"
+    datas = "{{mock 'cparagraph',length:15|section,end:2}}"
     print(decouple(datas))
--- application/ws/mock.py
+++ application/ws/mock.py
@@ -3,11 +3,16 @@
 
 from application import socketio, utils
 from flask import request
-from application.ws import online_server, session_maps
 
 
 mapping_dict = {
     'mock': utils.MockFaker()
+}
+
+
+function_mapping = {
+    'length': utils.length,
+    'section': utils.section
 }
 
 
@@ -16,10 +21,27 @@ def mock_data(body: dict):
     """ 通过表达式信息, 获取需要随机的数据 """
 
     expression = body.get('expression')
-    print(expression, '222')
+    print(expression, '2213123')
     classify, mapping, params, function = utils.decouple(expression)
-    print(classify, mapping, params, function, 333)
     result = getattr(mapping_dict[classify], mapping)(**params)
+    print(function, '2222')
+
+    for item in function:
+        params = dict()
+        if ',' in item:
+            function_params = item.split(',')
+            item = function_params[0]
+            _params = function_params[1:]
+
+            for param in _params:
+                key, value = param.split(":")
+                params[key] = value.replace("'", "") if "'" in value else int(value)
+
+        if item in function_mapping:
+            result = function_mapping[item](result, **params)
+        else:
+            result = utils.apply_string_function(result, item)
+
     session_id = getattr(request, 'sid', None)
 
     socketio.emit('reverseMockData', str(result), to=session_id)
